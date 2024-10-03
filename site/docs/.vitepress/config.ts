4a96f0cb8ef41fe81f34fba7372b534cd2266e43
Arie Timmerman <arietimmerman@gmail.com>
2024-10-03 21:07:09 +0200

    images

--- site/docs/.vitepress/config.ts
+++ site/docs/.vitepress/config.ts
@@ -2,14 +2,34 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
+    lastUpdated: true,
+    sitemap: {
+        hostname: 'https://verify.scim.dev'
+    },
     title: 'SCIM Verify',
+    titleTemplate: 'verify.scim.dev',
+    description: "Verify if your SCIM server meets the SCIM specification: a free CLI tool to verify SCIM server compliance",
+    head: [
+        [
+            'meta',
+            { property: 'og:image', content: '/scim-ogimage.png' }
+
+        ],
+        [
+            'link',
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon.png' }
+        ]
+    ],
     themeConfig: {
+        footer: {
+            copyright: 'Copyright © 2023-present <a href="https://www.limosa.io">Limosa Digital Identity Solutions</a>'
+        },
         nav: [
             { text: 'Home', link: '/' },
             { text: 'SCIM Playground', link: 'https://scim.dev', target: '_blank', rel: 'noopener' }
         ],
     },
     markdown: {
-        
+
     }
 })
