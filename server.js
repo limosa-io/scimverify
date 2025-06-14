0298784cefe8a9a799fe6369d5d3af5198f39b6f
Arie Timmerman <arietimmerman@gmail.com>
2025-06-14 11:23:26 +0200

    port 5000

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.post('/run-tests', async (req, res) => {
 if (process.env.SERVER_MODE !== 'function') {
     const httpServer = createServer(app);
     
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 5000;
     httpServer.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
